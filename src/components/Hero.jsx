5490e5cb8e585bfa60c2f3ab2ecf00bf7765ebc6
Vincent Ramos <vram1980@gmail.com>
2021-11-18 11:51:19 -0800

    adding css

---
+++ src/components/Hero.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="Hero">
+      <div className="container">
+        HERO Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odit sunt ea rem deleniti nobis inventore quis id nostrum vitae eos aliquam ab est sed deserunt corrupti, officia nulla animi aliquid.
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
