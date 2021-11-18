f541b7e9f6e8e25e2f02be9ed4e8cace209b8fa1
Vincent Ramos <vram1980@gmail.com>
2021-11-18 09:03:40 -0800

    basename changes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import {
 ReactDOM.render(
   <Router>
     <Navigation />
-    <Routes>
+    <Routes basename="/vincent-ramos">
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
