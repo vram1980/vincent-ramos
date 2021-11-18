db5493c77252117f73f7ae84d1dc345e74be91ca
Vincent Ramos <vram1980@gmail.com>
2021-11-18 09:28:35 -0800

    basename changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import {
   Navigation,
   Footer,
@@ -14,7 +15,7 @@ import {
 ReactDOM.render(
   <Router>
     <Navigation />
-    <Routes basename="/vincent-ramos">
+    <Routes basename="/">
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/contact" element={<Contact />} />
