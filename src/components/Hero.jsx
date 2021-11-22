f1015b016fff57a491dc0d4d4b7904b29be792d9
Vincent Ramos <vram1980@gmail.com>
2021-11-22 06:29:16 -0800

    added some images n stuff

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
               <div className="text-center  mt-5">
                 <h1 className="font-weight-bold  mb-0" style={{ letterSpacing: "-2px", color: "#535353" }}>Vincent Ramos</h1>
                 <h2 className="font-weight-bold  display-3  text-white  mb-3" style={{ letterSpacing: "-4px", lineHeight: "6rem" }}>UI<span className="position-relative  mx-2 font-weight-normal" style={{ top: "-7px" }}>/</span>UX&nbsp;&nbsp;Developer</h2>
-                <h4 className="font-weight-bold" style={{ color: "#e4ffe3" }}>Software engineer specializing on the client side. I build things using HTML5, CSS3, and JavaScript. I have worked on websites for the likes of Lexus, ABC, FX Networks, Vizio, Microsoft and Media Temple.</h4>
+                <h4 className="font-weight-normal" style={{ color: "#e4ffe3" }}>Software engineer specializing on the client side. I build things using HTML5, CSS3, and JavaScript. I have worked on websites for the likes of Lexus, ABC, FX Networks, Vizio, Microsoft and Media Temple.</h4>
               </div>
           </div>
       </div>
